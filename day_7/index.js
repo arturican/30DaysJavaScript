20edd3da8f9b8a788ea2e1f0e7c14948756b149e
arturican <arturican@gmail.com>
2022-02-21 19:54:57 +0300

    привел код в порядок с помощью eslint stylelint

--- day_7/index.js
+++ day_7/index.js
@@ -405,22 +405,6 @@ const reverseArray = (arr) => {
 
 reverseArray();
 
-const userIdGenerator = () => {
-  let symbol = prompt('Введите количество символов');
-  let generator = prompt('Введите количество генераций');
-  let str = 'ABCDEFGHIJKLMNOPQRSTUVWYZXabcdefghijklmnopqrstuvwxyz0123456789';
-  let str20 = '';
-  for (let j = 0; j <= generator - 1; j++) {
-    for (let i = 0; i <= symbol - 1; i++) {
-      str20 += str.charAt(Math.floor(Math.random() * (str.length - 1)));
-    }
-    str20 = str20 + '\n';
-  }
-
-  return console.log(str20);
-};
-userIdGenerator();
-
 const rgbColorGenerator = (count) => {
   let array = [];
   for (let i = 0; i <= count - 1; i++) {
--- day_7/index2.js
+++ day_7/index2.js
@@ -1,21 +1,15 @@
-const  userIdGenerator  = () => {
-    
-    let symbol = prompt('Введите количество символов');
-    let generator = prompt('Введите количество генераций');
-    
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWYZXabcdefghijklmnopqrstuvwxyz0123456789";
-    let str2 = '';
-    let str3 = '';
-       for (j = 0; j <= generator - 1; j++){
+const userIdGenerator = () => {
+  let symbol = prompt('Введите количество символов');
+  let generator = prompt('Введите количество генераций');
+  let str = 'ABCDEFGHIJKLMNOPQRSTUVWYZXabcdefghijklmnopqrstuvwxyz0123456789';
+  let str20 = '';
+  for (let j = 0; j <= generator - 1; j++) {
+    for (let i = 0; i <= symbol - 1; i++) {
+      str20 += str.charAt(Math.floor(Math.random() * (str.length - 1)));
+    }
+    str20 = str20 + '\n';
+  }
 
-        for (i = 0; i <= symbol - 1; i++){
-            str2 += str.charAt(Math.floor((Math.random() * (str.length - 1))));
-        }
-           str2 = str2+'\n';
-       }
-       
-            
-    return console.log(str2);
-}
-
+  return console.log(str20);
+};
 userIdGenerator();
