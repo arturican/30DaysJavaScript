fd2aba5613b09acedf5cb3a43080dea06ef53967
Artur <arturican@gmail.com>
2022-05-23 11:02:16 +0300

    userIdGenerator

--- day_7/index.js
+++ day_7/index.js
@@ -310,6 +310,12 @@ const randomHexaNumberGenerator = () => {
 
 randomHexaNumberGenerator();
 
+const userIdGenerator = () => {
+    let userId = Math.random().toString(36).substring(2,9);
+    return userId
+}
+userIdGenerator();
+
 const arrayOfHexaColors = (count) => {
     let hexa = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
     let array = [];
