b753b5b09b690966706a92e596da156837ca7b94
Artur <arturican@gmail.com>
2022-05-23 11:46:38 +0300

    переписал функцию rgbColorGenerator

--- day_7/index.js
+++ day_7/index.js
@@ -332,7 +332,16 @@ const userIdGeneratorSecond = () => {
   };
 
 userIdGeneratorSecond();
-  
+
+const rgbColorGenerator = () => {
+    let arr = [];
+    for (let i = 0; i < 3; i++){
+        arr.push(Math.floor(Math.random() * 255 + 1));
+    }
+    let rgbColor = 'rgb(' + arr.join() + ')' 
+    return rgbColor;
+}
+rgbColorGenerator ();
 
 const arrayOfHexaColors = (count) => {
     let hexa = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
