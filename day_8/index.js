83f6d5682863f019462bde0484255e53c36ec9b8
arturican <arturican@gmail.com>
2022-05-31 20:14:52 +0300

    добавил функцию signUp

--- day_8/index.js
+++ day_8/index.js
@@ -221,3 +221,119 @@ personAccount.addExpense(50, 'сыр');
 console.log(personAccount.totalIncomes());
 console.log(personAccount.totalExpense());
 console.log(personAccount.accountBalance());
+
+//3.2 **** Questions:2, 3 and 4 are based on the following two arrays:users and products ()
+//Imagine you are getting the above users collection from a MongoDB database.
+//a. Create a function called signUp which allows user to add to the collection.If user exists, inform the user that
+// he has already an account.
+//b. Create a function called signIn which allows user to sign in to the application
+
+const users = [
+    {
+        _id: 'ab12ex',
+        username: 'Alex',
+        email: 'alex@alex.com',
+        password: '123123',
+        createdAt:'08/01/2020 9:00 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'fg12cy',
+        username: 'Asab',
+        email: 'asab@asab.com',
+        password: '123456',
+        createdAt:'08/01/2020 9:30 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'zwf8md',
+        username: 'Brook',
+        email: 'brook@brook.com',
+        password: '123111',
+        createdAt:'08/01/2020 9:45 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'eefamr',
+        username: 'Martha',
+        email: 'martha@martha.com',
+        password: '123222',
+        createdAt:'08/01/2020 9:50 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'ghderc',
+        username: 'Thomas',
+        email: 'thomas@thomas.com',
+        password: '123333',
+        createdAt:'08/01/2020 10:00 AM',
+        isLoggedIn: false
+    }
+    ];
+
+    const products = [
+  {
+    _id: 'eedfcf',
+    name: 'mobile phone',
+    description: 'Huawei Honor',
+    price: 200,
+    ratings: [
+      { userId: 'fg12cy', rate: 5 },
+      { userId: 'zwf8md', rate: 4.5 }
+    ],
+    likes: []
+  },
+  {
+    _id: 'aegfal',
+    name: 'Laptop',
+    description: 'MacPro: System Darwin',
+    price: 2500,
+    ratings: [],
+    likes: ['fg12cy']
+  },
+  {
+    _id: 'hedfcg',
+    name: 'TV',
+    description: 'Smart TV:Procaster',
+    price: 400,
+    ratings: [{ userId: 'fg12cy', rate: 5 }],
+    likes: ['fg12cy']
+  }
+]
+
+const signUp = function (username, password, email) {
+  for(i = 0; i < users.length; i++){
+      if(users[i].username == username){
+          return console.log('Этот аккаунт уже существует');
+      } 
+  }
+  let _id = (Math.random() + 1).toString(36).substring(7);
+  for(i = 0; i < users.length; i++){
+  if(users[i]._id == _id){
+    _id = (Math.random() + 1).toString(36).substring(7);
+  }
+  }
+  const showDateTime = () => {
+    const now = new Date();
+    const year = now.getFullYear(); // return year
+    let month = now.getMonth() + 1; // return month(0 - 11)
+    const date = now.getDate(); // return date (1 - 31)
+    const hours = now.getHours(); // return number (0 - 23)
+    let minutes = now.getMinutes(); // return number (0 -59)
+    if (month < 10) {
+        month = month.toString().padStart(2, "0");
+    }
+    if (minutes < 10) {
+        minutes = minutes.toString().padStart(2, "0");
+    }
+    return `${date}/${month}/${year} ${hours}:${minutes}`;
+};
+let createdAt = showDateTime();
+
+
+
+ 
+ users.push({_id: _id, username: username, email: email, password: password, createdAt: createdAt})
+}
+signUp('Artur', '1243', 'arturican@gmail.com');
+signUp('Arturican', '1243', 'arturican@gmail.com');
